2950190e1a9b6d0f55042587e634bc71c75abd14
alexander windecker <waa_alex@hotmail.com>
2023-05-25 23:05:00 -0300

    documentacion api

--- src/persistence/mongoDB/models/carts.model.js
+++ src/persistence/mongoDB/models/carts.model.js
@@ -1,23 +1,23 @@
 import mongoose from "mongoose";
 
 const cartsSchema = new mongoose.Schema({
-  products: [
-    {
-      _id: false,
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Products",
-      },
-      quantity: {
-        type: Number,
-      },
-    },
-  ],
-});
+    products: [
+        {
+            _id: false,
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Products"
+            },
+            quantity: {
+                type: Number
+            }
+        }
+    ]
+})
 
-cartsSchema.pre("find", function (next) {
-  this.populate("products.product");
-  next();
-});
+cartsSchema.pre("find", function(next) {
+    this.populate("products.product")
+    next()
+})
 
-export const cartsModel = mongoose.model("Carts", cartsSchema);
+export const cartsModel = mongoose.model("Carts", cartsSchema)
